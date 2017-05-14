7f368a489e800dcdefb7e2876132a8f891e985de
oxygen-TW <howard2805@gmail.com>
2017-05-14 16:33:58 +0800

    新增安裝shell

---
+++ RPi software/install.sh
@@ -0,0 +1,5 @@
+sudo mkdir /usr/Weather_service
+sudo mv main_ACM0.py /usr/Weather_service
+sudo mv autorun.sh /usr/Weather_service
+sudo mkdir /usr/Weather_service/Data
+sudo touch /usr/Weather_service/Data/data.txt
