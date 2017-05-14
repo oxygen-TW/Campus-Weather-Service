99e324935a32fbd760ded55188f4e875c1ed2ab2
oxygen-TW <howard2805@gmail.com>
2017-05-14 16:49:17 +0800

    安裝shell更新

--- RPi software/install.sh
+++ RPi software/install.sh
@@ -1,5 +1,8 @@
 sudo mkdir /usr/Weather_service
-sudo mv main_ACM0.py /usr/Weather_service
-sudo mv autorun.sh /usr/Weather_service
+sudo mv RPi\ software/main_ACM0.py /usr/Weather_service
+sudo mv RPi\ software/autorun.sh /usr/Weather_service
 sudo mkdir /usr/Weather_service/Data
 sudo touch /usr/Weather_service/Data/data.txt
+sudo mv RPi\ software/croninfo /etc
+sudo crontab croninfo 
+
