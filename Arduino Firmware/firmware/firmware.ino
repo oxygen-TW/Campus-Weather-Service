c5d9ab82397c675e4b3888460d1cef9e73479697
oxygen-TW <howard2805@gmail.com>
2017-07-15 16:38:44 +0800

    新增細懸浮微粒感測韌體，未完成整合，UART佔用的問題沒解決

--- Arduino Firmware/firmware/firmware.ino
+++ Arduino Firmware/firmware/firmware.ino
@@ -1,15 +1,66 @@
+#include <SoftwareSerial.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+
 //General Firmware
 //Format
 //{"type":Firmware-Type,"obj1":Tempreture,"obj2":Humidity,"obj3":Light_index,"obj4":UV_index}
 //
 //2017/07/01
 
-#include <DHT.h>
 byte number = 0;
 #define DHTPIN 2
 #define DHTTYPE DHT22
+#define LENG 31   //0x42 + 31 bytes equal to 32 bytes
+unsigned char buf[LENG];
 
+int PM01Value=0;          //define PM1.0 value of the air detector module
+int PM2_5Value=0;         //define PM2.5 value of the air detector module
+int PM10Value=0;         //define PM10 value of the air detector module
+SoftwareSerial PMSerial(10,11);
 DHT sensor(DHTPIN, DHTTYPE);
+
+char checkValue(unsigned char *thebuf, char leng)
+{  
+  char receiveflag=0;
+  int receiveSum=0;
+
+  for(int i=0; i<(leng-2); i++){
+  receiveSum=receiveSum+thebuf[i];
+  }
+  receiveSum=receiveSum + 0x42;
+ 
+  if(receiveSum == ((thebuf[leng-2]<<8)+thebuf[leng-1]))  //check the serial data 
+  {
+    receiveSum = 0;
+    receiveflag = 1;
+  }
+  return receiveflag;
+}
+
+int transmitPM01(unsigned char *thebuf)
+{
+  int PM01Val;
+  PM01Val=((thebuf[3]<<8) + thebuf[4]); //count PM1.0 value of the air detector module
+  return PM01Val;
+}
+
+//transmit PM Value to PC
+int transmitPM2_5(unsigned char *thebuf)
+{
+  int PM2_5Val;
+  PM2_5Val=((thebuf[5]<<8) + thebuf[6]);//count PM2.5 value of the air detector module
+  return PM2_5Val;
+  }
+
+//transmit PM Value to PC
+int transmitPM10(unsigned char *thebuf)
+{
+  int PM10Val;
+  PM10Val=((thebuf[7]<<8) + thebuf[8]); //count PM10 value of the air detector module  
+  return PM10Val;
+}
 
 String MakeJson(double obj1, double obj2, double obj3, double obj4)
 {
@@ -38,34 +89,21 @@ String DtoS(double num)
 }
 
 
-/*-float TSPsensor()
-  {
-  int samplingTime = 280;
-  int deltaTime = 40;
-  int sleepTime = 180;
-
-  float voMeasured = 0;
-  float calcVoltage = 0;
-  float dustDensity = 0;
-  digitalWrite(ledPower, LOW); // power on the LED
-  delayMicroseconds(samplingTime);
-
-  voMeasured = analogRead(measurePin); // read the dust value
-
-  delayMicroseconds(deltaTime);
-  digitalWrite(ledPower, HIGH); // turn the LED off
-  delayMicroseconds(sleepTime);
-
-  // 0 - 5V mapped to 0 - 1023 integer values
-  // recover voltage
-  calcVoltage = voMeasured * (5.0 / 1024.0);
-  dustDensity = 0.17 * calcVoltage - 0.1;
-
-  return dustDensity*1000;
-  }*/
-
+void GetPMData()
+{
+  PMSerial.readBytes(buf,LENG);
+  Serial.println(buf[0]);
+  if(buf[0] == 0x4d){
+      if(checkValue(buf,LENG)){
+        PM01Value=transmitPM01(buf); //count PM1.0 value of the air detector module
+        PM2_5Value=transmitPM2_5(buf);//count PM2.5 value of the air detector module
+        PM10Value=transmitPM10(buf); //count PM10 value of the air detector module 
+      }          
+}
+}
 void setup() {
   Serial.begin(9600);
+  PMSerial.begin(9600);
   sensor.begin();
 }
 
@@ -74,6 +112,10 @@ void loop() {
     number = Serial.read();
     if (number == 'D')
     {
+      GetPMData();
+      Serial.println(PM01Value);
+      Serial.println(PM2_5Value);
+      Serial.println(PM10Value);
       double Temp = sensor.readTemperature();
       double Humi = sensor.readHumidity();
       double light = analogRead(A1);
