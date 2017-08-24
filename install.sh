6a35c314152bb5747238358b9f89122570c44004
oxygen-TW <oxygen-TW@users.noreply.github.com>
2017-08-24 17:03:43 +0800

    Fixed

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ sudo mv RPi\ software/main_ACM0.py /usr/Weather_service
 sudo mv RPi\ software/autorun.sh /usr/Weather_service
 sudo mkdir /usr/Weather_service/Data
 sudo touch /usr/Weather_service/Data/data.txt
-s#udo mv RPi\ software/crontab /etc
-#sudo crontab /etc/crontab
+sudo mv RPi\ software/crontab /etc
+sudo chmod +x /etc/crontab
+sudo crontab /etc/crontab
 echo Install completed
 echo Thank you!
